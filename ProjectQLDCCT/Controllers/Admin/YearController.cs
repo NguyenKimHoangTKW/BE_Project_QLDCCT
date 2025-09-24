a464a4f936116ff741d955a40151f7245d9d1d25
unknown <hoanggnk.it@gmail.com>
2025-09-24 14:23:15 +0700

    Them Module Year

--- ProjectQLDCCT/Controllers/Admin/YearController.cs
+++ ProjectQLDCCT/Controllers/Admin/YearController.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectQLDCCT.Data;
 using ProjectQLDCCT.Models;
+using ProjectQLDCCT.Models.DTOs;
 using System.Threading.Tasks;
 
 namespace ProjectQLDCCT.Controllers.Admin
 {
-    [Route("api/admin")]
+    [Route("api/admin/year")]
     [ApiController]
     public class YearController : ControllerBase
     {
@@ -40,5 +41,58 @@ namespace ProjectQLDCCT.Controllers.Admin
             }
 
         }
+        [HttpPost]
+        [Route("them-moi-nam-hoc")]
+        public async Task<IActionResult> ThemMoiNam([FromBody] YearsDTO items)
+        {
+            if (string.IsNullOrEmpty(items.name_year))
+            {
+                return Ok(new { message = "Không được bỏ trống Tên năm học", success = false });
+            }
+            var new_record = new Year
+            {
+                name_year = items.name_year,
+                time_cre = unixTimestamp,
+                time_up = unixTimestamp,
+            };
+            _context.Years.Add(new_record);
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Thêm dữ liệu thành công", success = true });
+        }
+        [HttpPost]
+        [Route("load-thong-tin-nam-hoc")]
+        public async Task<IActionResult> LoadInfo([FromBody] YearsDTO items)
+        {
+            var GetData = await _context.Years
+                .Where(x => x.id_year == items.value_year)
+                .Select(x => new
+                {
+                    value_year = x.id_year,
+                    name_year = x.name_year
+                })
+                .FirstOrDefaultAsync();
+            return Ok(GetData);
+        }
+        [HttpPost]
+        [Route("cap-nhat-nam-hoc")]
+        public async Task<IActionResult> CapNhatNamHoc([FromBody] YearsDTO items)
+        {
+            if (string.IsNullOrEmpty(items.name_year))
+            {
+                return Ok(new { message = "Không được bỏ trống Tên năm học", success = false });
+            }
+            var CheckItems = await _context.Years.FirstOrDefaultAsync(x => x.id_year == items.value_year);
+            CheckItems.name_year = items.name_year;
+            CheckItems.time_up = unixTimestamp;
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Chỉnh sửa dữ liệu thành công", success = true });
+        }
+        [HttpPost]
+        [Route("xoa-du-lieu-nam-hoc")]
+        public async Task<IActionResult> XoaNamHoc([FromBody] YearsDTO items)
+        {
+            return Ok();
+        }
+
     }
 }
---
+++ ProjectQLDCCT/Models/DTOs/YearsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectQLDCCT.Models.DTOs
+{
+    public class YearsDTO
+    {
+        public int value_year { get; set; }
+        public string name_year { get; set; }
+    }
+}
